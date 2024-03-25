e6b8f012795c601228ecf38b901a605543cac452
王军法 <junfa.wang@tencentmusic.com>
2024-03-25 17:56:06 +0800

    fix:修改代理配置调试部署

--- api/proxy.js
+++ api/proxy.js
@@ -1,13 +1,13 @@
 const { createProxyMiddleware, responseInterceptor } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  const version = '2.0.2';
+  const version = '2.0.3';
   console.log('version: ' + version);
   const host = req.headers.host; 
   const cookies = req.cookies;
 
 
-  let target = 'www.junfa.wang';
+  let target = 'www.baidu.com';
   if (host === 'gs.junfa.wang') {
     target = 'https://www.google.com/';
   } else if (host === 'ytb.junfa.wang') {
@@ -25,12 +25,17 @@ module.exports = (req, res) => {
     pathRewrite: {},
     selfHandleResponse: true,
     onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-      const response = responseBuffer.toString('utf8'); // convert buffer to string
-      return response
-        .replace(/https:\/\/www\.youtube\.com/gi, 'https://ytb.junfa.wang')
-        .replace(/https:\/\/www\.google\.com/gi, 'https://gs.junfa.wang')
-        .replace(/https:\/\/www\.github\.com/gi, 'https://hub.junfa.wang')
-        .replace(/<\/html>/gi, `<!-- proxy version: ${version} --> </html>`)
+      const contentType = proxyRes.headers['content-type'];
+      if (contentType && contentType.includes('text/')) {
+        const response = responseBuffer.toString('utf8');
+        return response
+          .replace(/https:\/\/www\.youtube\.com/gi, 'https://ytb.junfa.wang')
+          .replace(/https:\/\/www\.google\.com/gi, 'https://gs.junfa.wang')
+          .replace(/https:\/\/www\.github\.com/gi, 'https://hub.junfa.wang')
+          .replace(/<\/html>/gi, `<!-- proxy version: ${version} --> </html>`);
+      } else {
+        proxyRes.pipe(res);
+      }
     }),
   })(req, res);
 };
