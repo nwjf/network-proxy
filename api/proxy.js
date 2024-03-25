920a26c6c87f3f460015a0e4ac3155b2dbdd8641
王军法 <junfa.wang@tencentmusic.com>
2024-03-25 17:19:48 +0800

    feat:增加github，youtube代理

--- api/proxy.js
+++ api/proxy.js
@@ -1,24 +1,11 @@
 const { createProxyMiddleware, responseInterceptor } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  const version = '1.0.2';
+  const version = '2.0.1';
   console.log('version: ' + version);
-  // res.status(200).json({ name: 'Yongwang' })
-  // console.log('===');
-  // let target = "https://www.google.com";
-  // // let target = 'https://www.baidu.com';
-  // // 创建代理对象并转发请求
-  // return createProxyMiddleware({
-  //   target,
-  //   changeOrigin: true,
-  //   pathRewrite: {
-  //     // 通过路径重写，去除请求路径中的 `/backend`
-  //     // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
-  //     //   "^/backend/": "/",
-  //   },
-  // })(req, res);
   const host = req.headers.host; 
   const cookies = req.cookies;
+  console.log('host', host);
   // const { username, password } = cookies;
   // if (username !== 'admin' && password !== '9000') {
   //   res.redirect(301, '/next/login?type=logout'); 
@@ -26,23 +13,27 @@ module.exports = (req, res) => {
   // }
   // console.log('===');
 
-  let target = 'https://www.google.com/';
-  // let target = 'https://www.baidu.com/';
-  // let target = 'https://www.youtube.com/';
+  // let target = 'https://www.google.com/';
+  let target = 'www.junfa.wang';
+  if (host === 'gs.junfa.wang') {
+    target = 'https://www.google.com/';
+  } else if (host === 'ytb.junfa.wang') {
+    target = 'https://www.youtube.com/';
+  } else if (host === 'hub.junfa.wang') {
+    target = 'https://www.github.com';
+  }
   // // 创建代理对象并转发请求
   createProxyMiddleware({
     target,
     changeOrigin: true,
-    pathRewrite: {
-      // 通过路径重写，去除请求路径中的 `/backend`
-      // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
-      //   "^/backend/": "/",
-    },
+    pathRewrite: {},
     selfHandleResponse: true,
     onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
       const response = responseBuffer.toString('utf8') + `<!-- proxy version ${version} -->`; // convert buffer to string
       return response
         .replace(/https:\/\/www\.youtube\.com/gi, 'https://ytb.junfa.wang')
+        .replace(/https:\/\/www\.google\.com/gi, 'https://gs.junfa.wang')
+        .replace(/https:\/\/www\.github\.com/gi, 'https://hub.junfa.wang')
     }),
   })(req, res);
 };
